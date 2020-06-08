f1a6b6d42ba0253db1c842bcd1aa4e665fb06f89
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2020-06-08 18:20:48 +0900

    pratice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import PropType from "prop-types";
+
+const foodList = [{id:1,name:"a"},{id:2,name:"b"}];
+
+function Food({names}){
+return <h1>헷갈린다. {names}</h1>
+}
+
+function foodmap(dish){
+  return<Food names ={dish.name} />
+}
 
 function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+ return <div>{foodList.map(foodmap)}</div>;
 }
 
 export default App;
